ba297a6d3a5af38f90f772228f8b4c16fd9d690c
Daniël <daantjepietje@hotmail.com>
2025-01-15 17:35:35 +0100

    code voor het variabelen hoofdstuk

---
+++ variables/src/main.rs
@@ -0,0 +1,9 @@
+const OTHER_VALUE: u32 = 100 * 123;
+
+fn main() {
+    let mut x = 5;
+    println!("The value of x is {x}");
+    x = 6;
+    println!("The value of x is {x}");
+    println!("The constant value of OTHER_VALUE is {OTHER_VALUE}")
+}
