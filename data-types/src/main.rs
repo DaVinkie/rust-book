7633a531d5de309aedfa44156fca889f10a393e6
Daniël <daantjepietje@hotmail.com>
2025-01-16 16:35:06 +0100

    rekenen met numerics

--- data-types/src/main.rs
+++ data-types/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    numeric();
+}
+
+fn numeric() {
+    let addition = 423 + 355;
+    println!("423 + 355 = {addition}");
+    let subtraction = 123 - 456;
+    println!("123 - 456 = {subtraction}");
+    let multiplication = 34 * 123;
+    println!("34 * 123 = {multiplication}");
+    let division = 25.3 / 1.45;
+    println!("25.3 / 1.45 = {division}");
+    let int_division = 5 / 3;
+    println!("Trancated 5 / 3 = {int_division}");
+    let modulo = 29 % 6;
+    println!("29 % 6 = {modulo}");
 }
