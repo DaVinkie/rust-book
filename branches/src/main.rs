e6299b261c3510e237e5d653f9e7c37f20a5ca52
Daniël <daantjepietje@hotmail.com>
2025-01-20 14:41:23 +0100

    verschil mutables en shadowing

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -10,4 +10,38 @@ fn main() {
     } else {
         println!("Number is divisible by 1.");
     }
+
+    if_is_an_expression();
+    let_me_try_something();
+}
+
+
+fn if_is_an_expression() {
+    let condition = true;
+
+    let x = if condition { 4 } else { 3 };  // Expression can be used as assignment.
+
+    println!("The value of x is: {x}")
+}
+
+
+fn let_me_try_something() {
+    let condition = true;
+    let x;
+    
+    // x does not have to be mutable, because only 1 branch will be executed.
+    if condition {
+        x = 1;
+        println!("Value of x = {x} inside if-else");
+    } else {
+        x = 2;
+        println!("Value of x = {x} inside if-else");
+    }
+
+    {
+        let x = 10;  // Shadowing is different because we reuse `let`
+        println!("Shadowed value of x inside of new scope = {x}");    
+    }
+    // Value of x persists outside of if-else scope.
+    println!("Value of x outside of if-else scope = {x}");
 }
